1264d6317921d573d66542da4247f7f54d1a58c6
adriancho94 <adriancho94@9e822456-b5c3-4250-92de-b2e357365fb6>
2012-12-03 02:28:38 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/f22strikeforce/code@39 9e822456-b5c3-4250-92de-b2e357365fb6

--- F22-Strike-Force/src/principal.py
+++ F22-Strike-Force/src/principal.py
@@ -14,11 +14,11 @@ def main(num, punt=0):
 
     elif num == 3:        
         ventjuego = ventana.VentNivel(760, 710, 'fondo_nivel2.jpg','menu.mp3')        
-        ventjuego.correr(2, 1, 70, 0.5, [0.3, -0.3], 80 ,2, 210, punt, 600)
+        ventjuego.correr(2, 1, 70, 0.5, [0.3, -0.3], 80 ,2, 110, punt, 600)
 
     elif num == 4:        
         ventjuego = ventana.VentNivel(760, 710, 'fondo_nivel3.jpg','menu.mp3')
-        ventjuego.correr(3, 2, 90, 1, [0.4, -0.4], 60, 3, 320, punt, 850)
+        ventjuego.correr(3, 2, 90, 1, [0.4, -0.4], 60, 3, 120, punt, 850)
 
     elif num == 5:
         ventjuego = ventana.VentPuntajes(600, 490, 'fondo_menu.png','menu.mp3')
