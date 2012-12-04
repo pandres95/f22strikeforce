481482203620c377048dad82188abeaf26513aef
adriancho94 <adriancho94@9e822456-b5c3-4250-92de-b2e357365fb6>
2012-12-04 00:24:59 +0000

    documento de diseño actualizado(solo algunas cosas) nueva musica para jugar
    git-svn-id: svn+ssh://svn.code.sf.net/p/f22strikeforce/code@45 9e822456-b5c3-4250-92de-b2e357365fb6

--- F22-Strike-Force/src/principal.py
+++ F22-Strike-Force/src/principal.py
@@ -9,19 +9,19 @@ def main(num, punt=0):
         ventjuego.correr()
 
     elif num == 2:
-        ventjuego = ventana.VentNivel(760, 710, 'fondo_nivel1.png','menu.mp3')
+        ventjuego = ventana.VentNivel(760, 710, 'fondo_nivel1.png','juego.mp3')
         ventjuego.correr(1, 1, 50, 0.2, [0.2, -0.2], 100,1, 100, 0, 300)
 
     elif num == 3:        
-        ventjuego = ventana.VentNivel(760, 710, 'fondo_nivel2.jpg','menu.mp3')        
+        ventjuego = ventana.VentNivel(760, 710, 'fondo_nivel2.jpg','juego.mp3')        
         ventjuego.correr(2, 1, 70, 0.5, [0.3, -0.3], 80 ,2, 110, punt, 500)
 
     elif num == 4:        
-        ventjuego = ventana.VentNivel(760, 710, 'fondo_nivel3.jpg','menu.mp3')
+        ventjuego = ventana.VentNivel(760, 710, 'fondo_nivel3.jpg','juego.mp3')
         ventjuego.correr(3, 2, 90, 1, [0.4, -0.4], 60, 3, 120, punt, 850)
         
     elif num == 5:
-        ventjuego = ventana.VentNivel(760, 710, 'fondo_nivel4.png','menu.mp3')
+        ventjuego = ventana.VentNivel(760, 710, 'fondo_nivel4.png','juego.mp3')
         ventjuego.correr(4, 2, 500, 1, [0.2, -0.2], 40, 4, 150, punt, 1100)
 
     elif num == 6:
